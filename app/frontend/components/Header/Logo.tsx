91642144c408da959fefa82879a9f323de4c588f
Malik <malikfelix50@gmail.com>
2025-02-18 18:59:40 -0600

    Add Logo to login page

---
+++ app/frontend/components/Header/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Box, Typography} from "@mui/material";
+
+const Logo = () => {
+  return (
+    <Box sx={{ marginTop: 3 }}>
+      <Typography variant="h4" component="h1" gutterBottom sx={{fontWeight: '600'}}>{`Personifi`}</Typography>
+    </Box>
+
+  );
+};
+
+export default Logo;
